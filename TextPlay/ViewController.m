9cfefd4b61467103fbd1eca934a3817dc22b38e7
Princess Sampson <ps@kingonomics.com>
2016-08-30 00:26:41 -0400

    Will implement minimum and half star functionality

--- TextPlay/ViewController.m
+++ TextPlay/ViewController.m
@@ -8,15 +8,47 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+@property (nonatomic, strong) IBOutlet UILabel *leftLabel;
+@property (nonatomic, strong) IBOutlet UILabel *rightLabel;
+@property (strong, nonatomic) IBOutlet UIButton *leftButton;
+@property (strong, nonatomic) IBOutlet UIButton *rightButton;
+@property (strong, nonatomic) IBOutlet UIButton *clearButton;
+@property (nonatomic, copy) NSString *userText;
+
+-(IBAction)updateLabel:(UIButton*)sender;
+-(IBAction)clearLabels;
+-(void)disableLeftRightButtons;
 
 @end
 
 @implementation ViewController
 
+-(void)updateLabel:(UIButton *)sender {
+    if (sender == self.leftButton) {
+        self.leftLabel.text = self.userText;
+    } else if(sender == self.rightButton) {
+        self.rightLabel.text = self.userText;
+    }
+}
+
+-(IBAction)clearLabels {
+    self.leftLabel.text = @"";
+    self.rightLabel.text = @"";
+    [self disableLeftRightButtons];
+}
+
+-(void)disableLeftRightButtons {
+    self.leftButton.enabled = NO;
+    self.rightButton.enabled = NO;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self disableLeftRightButtons];
+    
+    self.userText = [NSString string];
 }
 
 
@@ -25,5 +57,25 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void)textFieldDidEndEditing:(UITextField *)textField
+                       reason:(UITextFieldDidEndEditingReason)reason {
+    
+    self.userText = textField.text;
+    self.leftButton.enabled = YES;
+    self.rightButton.enabled = YES;
+    
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField {
+    
+    BOOL shouldReturn = NO;
+    
+    if(textField.text.length != 0) {
+        [textField resignFirstResponder];
+        shouldReturn = YES;
+    }
+    
+    return shouldReturn;
+}
 
 @end
